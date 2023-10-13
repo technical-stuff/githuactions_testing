5921c0016ead404a376bbd96a96cc8c10378cb37
technical-stuff <container.images.com@gmail.com>
2023-10-13 17:56:51 +0530

    commit 11

--- Test/modules/launchtemplate/bin/userdata_script.sh
+++ Test/modules/launchtemplate/bin/userdata_script.sh
@@ -2,10 +2,10 @@
 sudo apt-get update
 sudo apt-get install -y git
 mkdir /home/ubuntu/actions-runner
-curl -o actions-runner-linux-x64-2.309.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.309.0/actions-runner-linux-x64-2.309.0.tar.gz -P /home/ubuntu/actions-runner
-echo "2974243bab2a282349ac833475d241d5273605d3628f0685bd07fb5530f9bb1a  actions-runner-linux-x64-2.309.0.tar.gz" | shasum -a 256 -c -P /home/ubuntu/actions-runner
-tar xzf /home/ubuntu/actions-runner/actions-runner-linux-x64-2.278.1.tar.gz
 cd /home/ubuntu/actions-runner
+sudo wget https://github.com/actions/runner/releases/download/v2.309.0/actions-runner-linux-x64-2.309.0.tar.gz
+sudo echo "2974243bab2a282349ac833475d241d5273605d3628f0685bd07fb5530f9bb1a  actions-runner-linux-x64-2.309.0.tar.gz" | shasum -a 256 -c -P /home/ubuntu/actions-runner
+sudo tar xzf actions-runner-linux-x64-2.309.0.tar.gz
 sudo ./config.sh --url https://github.com/technical-stuff/githuactions_testing --token A7XF7R4FDLGLNCMF72PC2WLFFD7KC
 sudo ./svc.sh install
 sudo ./svc.sh start
