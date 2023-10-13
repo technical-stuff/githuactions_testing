2638a6b49090235656082e9f4cb971961938d97c
technical-stuff <container.images.com@gmail.com>
2023-10-13 13:10:23 +0530

    commit3

--- Test/modules/launchtemplate/main.tf
+++ Test/modules/launchtemplate/main.tf
@@ -17,13 +17,14 @@ resource "aws_launch_template" "example" {
               apt-get update
               apt-get install -y git
               mkdir /home/ubuntu/actions-runner
+              wget https://github.com/actions/runner/releases/download/v2.278.1/actions-runner-linux-x64-2.278.1.tar.gz -P /home/ubuntu/actions-runner
+              tar xzf /home/ubuntu/actions-runner/actions-runner-linux-x64-2.278.1.tar.gz
               cd /home/ubuntu/actions-runner
-              wget https://github.com/actions/runner/releases/download/v2.278.1/actions-runner-linux-x64-2.278.1.tar.gz
-              tar xzf actions-runner-linux-x64-2.278.1.tar.gz
-              ./config.cmd --url https://github.com/technical-stuff/githuactions_testing --token A7XF7R4FDLGLNCMF72PC2WLFFD7KC
+              ./config.sh --url https://github.com/technical-stuff/githuactions_testing --token A7XF7R4FDLGLNCMF72PC2WLFFD7KC
               ./svc.sh install
-              ./svc.sh status
+              ./svc.sh start
               EOF)
+
 }
 
 
