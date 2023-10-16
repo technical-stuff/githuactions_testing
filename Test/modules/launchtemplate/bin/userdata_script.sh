302353a83f1ea930c64cc271f2cab9a03e3977d0
technical-stuff <container.images.com@gmail.com>
2023-10-16 10:27:25 +0530

    commit 12

--- Test/modules/launchtemplate/bin/userdata_script.sh
+++ Test/modules/launchtemplate/bin/userdata_script.sh
@@ -7,5 +7,6 @@ sudo wget https://github.com/actions/runner/releases/download/v2.309.0/actions-r
 sudo echo "2974243bab2a282349ac833475d241d5273605d3628f0685bd07fb5530f9bb1a  actions-runner-linux-x64-2.309.0.tar.gz" | shasum -a 256 -c -P /home/ubuntu/actions-runner
 sudo tar xzf actions-runner-linux-x64-2.309.0.tar.gz
 sudo ./config.sh --url https://github.com/technical-stuff/githuactions_testing --token A7XF7R4FDLGLNCMF72PC2WLFFD7KC
-sudo ./svc.sh install
-sudo ./svc.sh start
+./run.sh
+#sudo ./svc.sh install
+#sudo ./svc.sh start
